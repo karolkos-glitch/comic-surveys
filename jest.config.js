21ff0b0620dbe52d1554c6dae23cb66e9bdbc037
Karol <karol99kos@gmail.com>
2022-06-09 19:50:15 +0200

    Add  tests to CI

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '.+\\.(svg)$': 'jest-transform-stub',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/tests/jest-setup.ts']
+};
+
+module.exports = config;
---
+++ src/atoms/Checkbox/tests/utils.spec.ts
@@ -0,0 +1,9 @@
+import { getNumber } from "./utils"
+
+describe('getNumber', () => {
+  test('should get number', () => {
+    const number = getNumber(2);
+
+    expect(number).toEqual(2);
+  })
+})
