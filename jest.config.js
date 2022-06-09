6296d6d837ad1b782e8fb318b9a36025b8a0f0b3
Karol Kosek <56894071+karolkos-glitch@users.noreply.github.com>
2022-06-09 22:08:07 +0200

    Actions setups (#2)
    * Add some workflows
    
    * Adjust chromatic workflow
    
    * Add node.js
    
    * Add  tests to CI
    
    * Add tests to Ci
    
    * Adjust jest job
    
    * Adjust pipelines
    
    * add code coverage

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/**"],
+  coverageDirectory: 'coverage',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '.+\\.(svg)$': 'jest-transform-stub',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/tests/jest-setup.ts']
+};
+
+module.exports = config;
